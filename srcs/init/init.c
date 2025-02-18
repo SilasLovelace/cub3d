a83711121a596a16a8f7647945a68100450777dc
Silas Oppermann <silas.oppermann@gmail.com>
2025-02-18 15:21:52 +0100

    inint norminetted

--- srcs/init/init.c
+++ srcs/init/init.c
@@ -6,12 +6,23 @@
 /*   By: sopperma <sopperma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 13:20:42 by tkafanov          #+#    #+#             */
-/*   Updated: 2025/02/18 12:37:46 by sopperma         ###   ########.fr       */
+/*   Updated: 2025/02/18 15:20:53 by sopperma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../../includes/cub3d.h"
+
+t_memory	*free_addr(t_memory *memory, int i)
+{
+	if (i > 0)
+		free(memory->resources);
+	if (i > 1)
+		free(memory->player_pos);
+	if (i > 2)
+		free(memory->mlx_data);
+	free(memory);
+	return (NULL);
+}
 
 t_memory	*get_memory(void)
 {
@@ -19,45 +30,21 @@ t_memory	*get_memory(void)
 
 	if (!memory)
 	{
-		memory = malloc(sizeof(t_memory));
+		memory = calloc(sizeof(t_memory), 1);
 		if (!memory)
 			return (NULL);
-		ft_bzero(memory, sizeof(t_memory));
-		memory->resources = malloc(sizeof(t_map_resources));
+		memory->resources = calloc(sizeof(t_map_resources), 1);
 		if (!memory->resources)
-		{
-			free(memory);
-			return (NULL);
-		}
-		ft_bzero(memory->resources, sizeof(t_map_resources));
-		memory->player_pos = malloc(sizeof(t_coordinates));
+			return (free_addr(memory, 0));
+		memory->player_pos = calloc(sizeof(t_coordinates), 1);
 		if (!memory->player_pos)
-		{
-			free(memory->resources);
-			free(memory);
-			return (NULL);
-		}
-		ft_bzero(memory->player_pos, sizeof(t_coordinates));
-		memory->mlx_data = malloc(sizeof(t_mlx_data));
+			return (free_addr(memory, 1));
+		memory->mlx_data = calloc(sizeof(t_mlx_data), 1);
 		if (!memory->mlx_data)
-		{
-			free(memory->resources);
-			free(memory->player_pos);
-			free(memory);
-			return (NULL);
-		}
-		ft_bzero(memory->mlx_data, sizeof(t_mlx_data));
-		memory->keys = malloc(sizeof(t_keys));
+			return (free_addr(memory, 2));
+		memory->keys = calloc(sizeof(t_keys), 1);
 		if (!memory->keys)
-		{
-			free(memory->resources);
-			free(memory->player_pos);
-			free(memory->mlx_data);
-			// free(memory->mlx_data->textures);
-			free(memory);
-			return (NULL);
-		}
-		ft_bzero(memory->keys, sizeof(t_keys));
+			return (free_addr(memory, 3));
 		memory->mlx_data->resolution_x = 1920;
 		memory->mlx_data->resolution_y = 1080;
 		memory->resources->ceiling_color = -1;
